2003c0286a834cb861be5af781cc16e52e50aaef
armanahmed001 <armanahmed001cbst@gmail.com>
2019-10-14 19:57:59 +0600

    Practices Project

---
+++ Assignment8/Assignment8/Model/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8.Model
+{
+    public class Customer
+    {
+        public int Id  {set; get; }
+        public string Name {set; get; }
+        public string Address {set; get; }
+        public string Contact  {set; get; }
+    }
+}
